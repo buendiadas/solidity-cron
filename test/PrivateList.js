63ec74d00c81482bed2e086da419f524fd540111
buendiadas <buendiacarlos@gmail.com>
2018-02-01 12:28:22 +0100

    addition and contract creation tests

--- test/PrivateList.js
+++ test/PrivateList.js
@@ -1,7 +1,7 @@
 const expectThrow = require('./helpers/expectThrow.js').expectThrow;
 const Standard20TokenContract = artifacts.require('Standard20Token');
 const PrivateListContract = artifacts.require('PrivateList');
-const MAXNUMCANDIDATES=10;
+const MAXNUMCANDIDATES=5;
 
 let PrivateList;
 let FrontierToken;
@@ -10,42 +10,66 @@ let FrontierToken;
 contract('PrivateList', function (accounts) {
   beforeEach(async() => {
     FrontierToken = await Standard20TokenContract.new();
-    PrivateList = await PrivateListContract.new(await FrontierToken.address, MAXNUMCANDIDATES,{from:accounts[0]});
+    let tokenAddress= await FrontierToken.address;
+    PrivateList = await PrivateListContract.new(tokenAddress, MAXNUMCANDIDATES,{from:accounts[0]});
   })
 
-  it('Should have set as an owner the creator of the List', async () => {
-    let owner = await PrivateList.owner.call();
-    assert.strictEqual(accounts[0], owner);
-  })
+  describe('Creating the contract', async () => {
 
-  it('Should have the total number of candidates to the specified in constructor', async () => {
-    let numCandidates = await PrivateList.maxNumCandidates();
-    assert.strictEqual(MAXNUMCANDIDATES, await numCandidates);
-  })
+      it('Should have set as an owner the creator of the List', async () => {
+        let owner = await PrivateList.owner.call();
+        assert.strictEqual(accounts[0], owner);
+      })
 
+      it('Should have the total number of candidates to the specified in constructor', async () => {
+        let numCandidates = await PrivateList.maxNumCandidates.call();
+        assert.equal(MAXNUMCANDIDATES, await numCandidates);
+      })
+  });
 
-  it('Should add a candidate if it is required by the owner', async () => {
-    let newCandidate= accounts[1];
-    await PrivateList.addCandidate(newCandidate);
-    let isInTheList = await PrivateList.candidatesList.call(newCandidate);
-    assert.strictEqual(true, isInTheList);
-  })
+  describe('Adding candidates', async () => {
 
-  it('Should NOT add a candidate if it is required by an account different than the owner', async () => {
-    let newCandidate= accounts[1];
-    await expectThrow(PrivateList.addCandidate(newCandidate,{from:accounts[2]}))
-  })
+      it('Should add a candidate if it is required by the owner', async () => {
+        let newCandidate= accounts[1];
+        await PrivateList.addCandidate(newCandidate);
+        let isInTheList = await PrivateList.candidatesList.call(newCandidate);
+        assert.strictEqual(true, isInTheList);
+      })
 
-  it('Should increase the analysts counter after adding a new candidate ', async () => {
-    let initialNumberOfCandidates= await PrivateList.candidateCounter.call();
-    let newCandidate= await accounts[2];
-    await PrivateList.addCandidate(newCandidate);
-    newCandidate= await accounts[3];
-    await PrivateList.addCandidate(newCandidate);
-    let updatedNumberOfCandidates= await PrivateList.candidateCounter.call();
-    assert.strictEqual(initialNumberOfCandidates + 1, updatedNumberOfCandidates);
-  })
+      it('Should NOT add a candidate if it is required by an account different than the owner', async () => {
+        let newCandidate= accounts[1];
+        await expectThrow(PrivateList.addCandidate(newCandidate,{from:accounts[2]}))
+      })
 
+      it('Should increase the analysts counter after adding a new candidate ', async () => {
+        let initialNumberOfCandidates= await PrivateList.candidateCounter.call();
+        let newCandidate= await accounts[2];
+        await PrivateList.addCandidate(newCandidate);
+        let updatedNumberOfCandidates= await PrivateList.candidateCounter.call();
+        assert.equal(1, updatedNumberOfCandidates);
+      })
+
+      it('Should increase the analysts counter N times before the MAX is reached', async () => {
+        let initialNumberOfCandidates= await PrivateList.candidateCounter.call();
+        let newCandidate= await accounts[2];
+        let i = 0;
+        while (i< MAXNUMCANDIDATES){
+          await PrivateList.addCandidate(accounts[i+2]);
+          i++;
+        }
+        let updatedNumberOfCandidates= await PrivateList.candidateCounter.call();
+        assert.equal(MAXNUMCANDIDATES, updatedNumberOfCandidates);
+      })
+
+      it('Should throw if the analyst is added twice', async () => {
+        let initialNumberOfCandidates= await PrivateList.candidateCounter.call();
+        let newCandidate= await accounts[2];
+        await PrivateList.addCandidate(newCandidate);
+        await expectThrow(PrivateList.addCandidate(newCandidate));
+        let updatedNumberOfCandidates= await PrivateList.candidateCounter.call();
+        assert.equal(1, updatedNumberOfCandidates);
+      })
+    });
 
 
 
