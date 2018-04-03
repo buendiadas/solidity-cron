41ad7859b03161f56ef978e93bafffa53b02725c
buendiadas <buendiacarlos@gmail.com>
2018-04-03 19:40:21 +0200

    Remove private key config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 let config={}
 
-config.admin_account_priv = web3.eth.accounts.privateKeyToAccount(process.env.TRL_ADMIN_KEY)
 config.ttl = 10000
 config.candidate_length = 100
 config.voter_length = 100
