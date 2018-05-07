58b118e62ab6c495a4cfa15d1b7742f03d9c24a5
Carlos Buendía <cbuendia@influity.com>
2018-05-08 00:15:42 +0200

    Added Factory pattern on Registry deployment

---
+++ contracts/OwnedRegistryFactory.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.4.19;
+
+import "./OwnedRegistry.sol";
+
+/**
+* Generic Registry, used for Candidates and Voters
+*
+**/
+
+contract OwnedRegistryFactory{
+
+    /**
+    * Creates a new Owned Registry
+    * @param _maxNumListings Maximum number of listings for the created registry
+    *
+    */
+    function newRegistry(uint256 _maxNumListings) public returns (OwnedRegistry _reg){
+        OwnedRegistry reg = new OwnedRegistry(_maxNumListings);
+        OwnedRegistryCreation(msg.sender);
+        return reg;
+    }
+
+    event OwnedRegistryCreation(address indexed creator);
+
+}
