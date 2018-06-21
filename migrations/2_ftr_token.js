c427737b6bad901c03918400c0d1f39efc7d82ef
Carlos Buendía <cbuendia@influity.com>
2018-06-21 13:25:24 +0200

    chore(travis): Removes web3 sync requests from migrations, moving accounts to the config file

--- migrations/2_ftr_token.js
+++ migrations/2_ftr_token.js
@@ -1,6 +1,7 @@
 const config = require('../config')
 const Standard20TokenContract = artifacts.require('Standard20TokenMock')
 
+
 module.exports = (deployer) => {
-  deployer.deploy(Standard20TokenContract, web3.eth.accounts, config.initialBalance)
+  deployer.deploy(Standard20TokenContract, config.voterAccounts, config.initialBalance)
 }
--- migrations/4_trl.js
+++ migrations/4_trl.js
@@ -3,8 +3,7 @@ const OwnedRegistryFactory = artifacts.require('OwnedRegistryFactory')
 const OwnedRegistryContract = artifacts.require('OwnedRegistry')
 const Standard20TokenContract = artifacts.require('Standard20TokenMock')
 const TRLContract = artifacts.require('TRL')
-const voterAccounts = web3.eth.accounts.slice(1, 4)
-const candidateAccounts = web3.eth.accounts.slice(5, 8)
+
 
 const initializeRegistry = function (registry, inputAccounts) {
   for (let i = 0; i < inputAccounts.length; i++) {
@@ -16,9 +15,9 @@ module.exports = (deployer) => {
   deployer.then(async () => {
     const RegistryFactory = await OwnedRegistryFactory.deployed()
     const voterRegistryAddress = await RegistryFactory.newRegistry.call(config.voterLength)
-    await initializeRegistry(OwnedRegistryContract.at(voterRegistryAddress), voterAccounts)
+    await initializeRegistry(OwnedRegistryContract.at(voterRegistryAddress), config.voterAccounts)
     const candidateRegistryAddress = await RegistryFactory.newRegistry.call(config.voterLength)
-    await initializeRegistry(OwnedRegistryContract.at(candidateRegistryAddress), candidateAccounts)
+    await initializeRegistry(OwnedRegistryContract.at(candidateRegistryAddress), config.candidateAccounts)
     const FrontierToken = await Standard20TokenContract.deployed()
     await deployer.deploy(TRLContract, FrontierToken.address, candidateRegistryAddress, voterRegistryAddress, config.activeTime, config.claimTime, config.ttl)
   })
