afcbf9c9a2b021d9bd52c0d0ed892b6d98703e65
Joao Santos <jrmsantos15@gmail.com>
2018-09-17 16:39:39 +0100

    test(.travis.yml): Tests travis build

---
+++ small-setup.sh
@@ -0,0 +1,35 @@
+# from TRL folder
+
+# START rabbitqm
+
+# export TRAVIS_BUILD_DIR=/Users/boss/git/frontier/trl-project/TRL
+export PROXY_ADDR_PATH=$TRAVIS_BUILD_DIR/../trlProxyAddress.json
+
+
+# setup frontierjs
+mkdir $TRAVIS_BUILD_DIR/../frontierjs-dev && cd $TRAVIS_BUILD_DIR/../frontierjs-dev
+git https://github.com/Frontier-project/frontier-js.git
+cd $TRAVIS_BUILD_DIR/../frontierjs-dev/frontier-js
+npm install
+npm run start:testrpc &
+npm run test
+
+
+
+# rm -r dist
+# #pass env variable here!
+# npm run build-dist
+
+# # setup trl-listener
+
+# mkdir $TRAVIS_BUILD_DIR/../trl-listener-dev && cd $TRAVIS_BUILD_DIR/../trl-listener-dev
+# git clone git@github.com:Frontier-project/trl-listener.git
+# cd $TRAVIS_BUILD_DIR/../trl-listener-dev/trl-listener
+# npm install
+# npm link $TRAVIS_BUILD_DIR/../frontierjs-dev/frontier-js
+# npm run build
+# npm run start:testrpc &
+# npm run test
+
+# #ganache-cli &
+# #npm run test
