9ae7b0ccc87cf4536e4d0a57884d1d417ab7cf5e
Joao Santos <jrmsantos15@gmail.com>
2018-09-18 16:30:49 +0100

    ci(travis): tests new pipeline

--- small-setup.sh
+++ small-setup.sh
@@ -3,9 +3,16 @@
 # START rabbitqm
 
 #export TRAVIS_BUILD_DIR=/Users/boss/git/frontier/trl-project/TRL
+echo "----------------------------------"
+echo "----------------------------------"
+echo "----------------------------------"
+echo "--- STARTING INTEGRATION TEST ----"
+echo "----------------------------------"
 export PLAYGROUND_FRONTIERJS=$TRAVIS_BUILD_DIR/../frontierjs-dev
 export PLAYGROUND_TRLLISTENER=$TRAVIS_BUILD_DIR/../trllistener-dev
 export PROXY_ADDR_PATH=$TRAVIS_BUILD_DIR/../trlProxyAddress.json
+
+export INTEGRATION_TEST=1
 
 # setup frontierjs
 mkdir $PLAYGROUND_FRONTIERJS
@@ -16,8 +23,11 @@ echo "Going to playground!"
 git clone --single-branch -b FRN-130/test/continuos-integration-trl https://github.com/Frontier-project/frontier-js.git $PLAYGROUND_FRONTIERJS
 ( cd $PLAYGROUND_FRONTIERJS && echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc )
 ( cd $PLAYGROUND_FRONTIERJS && npm install )
-( cd $PLAYGROUND_FRONTIERJS && npm run start:testrpc & )
-( cd $PLAYGROUND_FRONTIERJS && npm run test )
+#( cd $PLAYGROUND_FRONTIERJS && npm run start:testrpc & )
+#( cd $PLAYGROUND_FRONTIERJS && npm run test )
+#Clean build
+#( cd $PLAYGROUND_FRONTIERJS && rm -r dist )
+#( cd $PLAYGROUND_FRONTIERJS && npm run build-dist )
 
 # rm -r dist
 # #pass env variable here!
@@ -36,5 +46,6 @@ git clone --single-branch -b FRN-130/test/continuos-integration-trl https://gith
  ( cd $PLAYGROUND_TRLLISTENER && npm run start:testrpc & )
  ( cd $PLAYGROUND_TRLLISTENER && npm run test )
 
+export INTEGRATION_TEST=0
 cd $TRAVIS_BUILD_DIR
 echo "Finished small-setup"
