30447edd0fa564b84f0f4222949448698b1651ce
Carlos Buendía <cbuendia@influity.com>
2018-09-25 14:34:36 +0200

    fix(TRL Interface): Removes unused CompensationRelease event

--- contracts/TRLInterface.sol
+++ contracts/TRLInterface.sol
@@ -8,11 +8,9 @@ contract TRLInterface {
 
     event PeriodInit(uint256 _T, uint256 _active, uint256 _claim);
     event VotesBought(address indexed _recipient, uint256 _amount, uint256 _epoch);
-    event CompensationReleased(address indexed _recipient, uint256 _amount, uint256 _epoch);
     event MinimumStakeSet(uint256 _amount);
     event Vote(address indexed _voterAddress, address indexed _candidateAddress, uint256 _amount, uint256 _epoch);
     event PeriodicStagesCreated(address _a);
-
 
     function vote(address _candidateAddress, uint256 _amount) external;
     function height() public view returns(uint256);
