70158e24646ce597debdb82dbf59004d1d77e7a4
Carlos Buendía <cbuendia@influity.com>
2018-09-25 13:20:39 +0200

    ci(migrations): Includes Vault into migration pipeline

--- migrations/5_registries.js
+++ migrations/5_registries.js
@@ -3,7 +3,6 @@ const Standard20TokenContract = artifacts.require('Standard20TokenMock')
 const keccak256 = require('js-sha3').keccak256
 const OwnedRegistryFactoryContract = artifacts.require('@frontier-token-research/role-registries/contracts/OwnedRegistryFactory')
 
-
 module.exports = (deployer) => {
   deployer.then(async () => {
     if (config.proxyMigration) {
