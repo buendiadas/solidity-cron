42ea27b37cfe71b1f90cd6123ec1a33c61209751
Joao Santos <jrmsantos15@gmail.com>
2018-11-20 20:29:38 +0000

    feat(Balance): Adds Balance management feature
    The balance is kept for each entity based on its Allowance. It calculates the amount of tokens each
    entity is allowed to withdraw, and updates its balance.

--- contracts/Balance.sol
+++ contracts/Balance.sol
@@ -9,68 +9,78 @@ import "./Vault.sol";
 
 
 contract Balance is Ownable {
-    using SafeMath for uint256;
+		using SafeMath for uint256;
 
-    TRL TRLInstance;
-    Allowance AllowanceInstance;
-    Vault VaultInstance;
+		TRL TRLInstance;
+		Allowance AllowanceInstance;
+		Vault VaultInstance;
 
-    //        period              token             entity      balance  
-    mapping (uint256 => mapping (address => mapping(address => uint256))) entityBalanceForPeriod;
-    mapping (uint256 => mapping (address => uint256)) public balanceStage; // 0 -> unset , 1 -> set, 2 -> changed
+		//        period              token             entity      balance  
+		mapping (uint256 => mapping (address => mapping(address => uint256))) entityBalanceForPeriod;
+		mapping (uint256 => mapping (address => uint256)) public balanceStage; // 0= unset , 1= set, 2= changed
 
-    constructor (address _TRLContractAddress, address _allowanceContractAddress, address _vaultContractAddress) public {
-        require(msg.sender == owner(), "Sender must be the owner");
-        TRLInstance = TRL(_TRLContractAddress);
-        AllowanceInstance = Allowance(_allowanceContractAddress);
-        VaultInstance = Vault(_vaultContractAddress);
-    }
+		constructor (address _TRLContractAddress, address _allowanceContractAddress, address _vaultContractAddress) public {
+				require(msg.sender == owner(), "Sender must be the owner");
+				TRLInstance = TRL(_TRLContractAddress);
+				AllowanceInstance = Allowance(_allowanceContractAddress);
+				VaultInstance = Vault(_vaultContractAddress);
+		}
 
-    // anyone can call this function
-    function setBalancesForEntities(address[] _entities, address _tokenAddress) external{
-        // only allow 10 at most, more will probably be an input mistake
-        require(_entities.length < 10,"Should not provide more than 10 entities");
-        uint256 currentPeriod = TRLInstance.height();
-        uint256 periodPool = VaultInstance.balance(currentPeriod,_tokenAddress);
+		// anyone can call this function
+		function setBalancesForEntities(address[] _entities, address _tokenAddress) external{
+				// only allow 10 at most, more will probably be an input mistake
+				require(_entities.length < 10,"Should not provide more than 10 entities");
+				// Geting the current period from TRL
+				uint256 currentPeriod = TRLInstance.height();
+				// Getting the ammount of tokens in the Bounty Poll
+				uint256 periodPool = VaultInstance.balance(currentPeriod,_tokenAddress);
 
-        for(uint256 i = 0; i<_entities.length; i++){
-            // balance(uint256 _vaultID, address _token)
-            uint256 entityAllowance = AllowanceInstance.getEntityAllowance(_entities[i]);
-            uint256 entityAbsoluteAllowance = _calculateBalance(entityAllowance, periodPool);
-            entityBalanceForPeriod[currentPeriod][_tokenAddress][_entities[i]] = entityAbsoluteAllowance;
-            balanceStage[currentPeriod][_tokenAddress] = 1;
-        }
-    }
-    // updates the balance after a withdrawal
-    function withdraw(address _entity, address _tokenAddress, uint256 ammountWithdrawn) external {
-        require(msg.sender == owner() || msg.sender == _entity,"Only the owner can update this value");
-        uint256 currentPeriod = TRLInstance.height();
-        uint256 currentBalance = entityBalanceForPeriod[currentPeriod][_tokenAddress][_entity];
-        require(ammountWithdrawn <= currentBalance, "Trying to withdraw more than the balance");
+				//Calculate the balance for each entity, based on its allowance
+				for(uint256 i = 0; i<_entities.length; i++){
+						// Get the entity's allowance in Percentage form
+						uint256 entityAllowance = AllowanceInstance.getEntityAllowance(_entities[i]);
+						// Get the entity's allowance in number of Tokens, based on the ammount
+						// of tokens in the bounty pool
+						uint256 entityAbsoluteAllowance = _calculateBalance(entityAllowance, periodPool);
+						// Set the entity's balance for the current period as the number of Tokens
+						entityBalanceForPeriod[currentPeriod][_tokenAddress][_entities[i]] = entityAbsoluteAllowance;
+						// Set the balance stage flag to 1, meaning "set".
+						// "set" means the balance has been calculated and had not been changed.
+						balanceStage[currentPeriod][_tokenAddress] = 1;
+				}
+		}
+		// updates the balance after a withdrawal
+		function withdraw(address _entity, address _tokenAddress, uint256 ammountWithdrawn) external {
+				// check that it's the owner calling the function    
+				require(msg.sender == owner() || msg.sender == _entity,"Only the owner can update this value");
+				// Get the current period value from TRL
+				uint256 currentPeriod = TRLInstance.height();
+				// Get the current balance of this entity, in number of tokens
+				uint256 currentBalance = entityBalanceForPeriod[currentPeriod][_tokenAddress][_entity];
+				// Check that it's not withdrawing more than it has
+				require(ammountWithdrawn <= currentBalance, "Trying to withdraw more than the balance");
+				// Update the entity's balance
+				entityBalanceForPeriod[currentPeriod][_tokenAddress][_entity] = currentBalance.sub(ammountWithdrawn);
+				// Set the balance stage flag to 2, meaning "changed".
+				// "changed" means the balance has been changed after it was set.    
+				balanceStage[currentPeriod][_tokenAddress] = 2;
+		}
+		// Get balance for current period
+		function getBalance (address _entity, address _tokenAddress) view external returns (uint256){
+				uint256 currentPeriod = TRLInstance.height();
+				return entityBalanceForPeriod[currentPeriod][_tokenAddress][_entity];
+		}
 
-        entityBalanceForPeriod[currentPeriod][_tokenAddress][_entity] = currentBalance.sub(ammountWithdrawn);
-        balanceStage[currentPeriod][_tokenAddress] = 2;
-    }
+		// Get balance for specific period
+		/*
+		   function getBalance (address _entity, address _tokenAddress, uint256 _period) view external returns (uint256){
+		   return entityBalanceForPeriod[_period][_tokenAddress][_entity];
+		   }*/
 
-    function getBalance (address _entity, address _tokenAddress) view external returns (uint256){
-        uint256 currentPeriod = TRLInstance.height();
-        return entityBalanceForPeriod[currentPeriod][_tokenAddress][_entity];
-    }
 
-    function getBalance (address _entity, address _tokenAddress, uint265 _period) view external returns (uint256){
-        uint256 currentPeriod;
-        if(!_period){
-            currentPeriod = TRLInstance.height();
-        }else{
-            currentPeriod = _period;
-        }
-        return entityBalanceForPeriod[currentPeriod][_tokenAddress][_entity];
-    }
-
-    
-
-    function _calculateBalance(uint256 _entityAllowance, uint256 _periodPool) public pure returns (uint256 allowance){
-        uint256 stepCalculation = _entityAllowance.mul(_periodPool);
-        return stepCalculation.div(100);
-    }
+		//Returns the number of tokens, based on the percentage share of the bounty pool tokens 
+		function _calculateBalance(uint256 _entityAllowance, uint256 _periodPool) public pure returns (uint256 allowance){
+				uint256 stepCalculation = _entityAllowance.mul(_periodPool);
+				return stepCalculation.div(100);
+		}
 }
--- test/Balance.js
+++ test/Balance.js
@@ -9,45 +9,102 @@ const TRLContract = artifacts.require('TRL')
 const VaultContract = artifacts.require('Vault')
 
 contract('TRL<Active>', function (accounts) {
-  let ProxyInstance
-  let TRLInstance
-  let FrontierTokenInstance
-  let Vault
-  let startTime
-  let owner = web3.eth.accounts[0]
-  let voterAccounts = web3.eth.accounts.slice(1, 4)
-  let candidateAccounts = web3.eth.accounts.slice(5, 8)
-  let Allowance
-  let Balance
+		let ProxyInstance
+		let TRLInstance
+		let FrontierTokenInstance
+		let Vault
+		let startTime
+		let owner = web3.eth.accounts[0]
+		let voterAccounts = web3.eth.accounts.slice(1, 4)
+		let candidateAccounts = web3.eth.accounts.slice(5, 8)
+		let Allowance
+		let Balance
 
-  before('Deploying required contracts', async () => {
-    FrontierTokenInstance = await Standard20TokenMock.new(voterAccounts, config.totalTokens, {from: owner})
-    Vault = await VaultContract.new({from: owner})
-  })
-  beforeEach(async () => {
-    TRLInstance = await TRLContract.new({from: owner})
-    await TRLInstance.setToken(FrontierTokenInstance.address)
-    await TRLInstance.setVault(Vault.address)
-    await TRLInstance.initPeriod(config.ttl)
-    await TRLInstance.initStages(config.activeTime, config.claimTime)
+		const entity1 = voterAccounts[0]
+		const entity2 = voterAccounts[1]
+		const entity3 = voterAccounts[2]
+		const totalTokenIssuance = 100*1000
 
-    Allowance = await AllowanceContract.new()
-    Balance = await BalanceContract.new(TRLInstance.address, Allowance.address, Vault.address)
-  })
+		const entity1Allowance = 50
+		const entity2Allowance = 20
+		const entity3Allowance = 10
 
-  it('Should calculate the balance', async() => {
-      // _calculateBalance(uint256 _entityAllowance, uint256 _periodPool)
-    const entityAllowance = 40
-    const periodPool = 200
-    const expectedAllowance = 80
-    const actualAllowance = await Balance._calculateBalance(entityAllowance, periodPool)
-    assert.equal(actualAllowance, expectedAllowance)
-  })
+		const impossibleAllowance = 101
 
-  it('Should be period 0', async() => {
-    // _calculateBalance(uint256 _entityAllowance, uint256 _periodPool)
+		let period = 0
 
-    const balance = await Balance.getBalance(candidateAccounts[0], FrontierTokenInstance.address)
-    assert.equal(balance, 0)
-  })
+		beforeEach(async () => {
+
+				// creting token instance
+				FrontierTokenInstance = await Standard20TokenMock.new(voterAccounts, totalTokenIssuance, {from: owner})
+				// Creating Vault
+				Vault = await VaultContract.new({from: owner})
+				// Approving trasfer to fund Vault
+				await FrontierTokenInstance.approve(Vault.address, totalTokenIssuance, {from: voterAccounts[0]})
+				// Funding Vault
+				await Vault.deposit(0, FrontierTokenInstance.address, voterAccounts[0], totalTokenIssuance, {from: voterAccounts[0]})
+
+				await Vault.close(0, FrontierTokenInstance.address)
+				TRLInstance = await TRLContract.new({from: owner})
+				await TRLInstance.setToken(FrontierTokenInstance.address)
+				await TRLInstance.setVault(Vault.address)
+				await TRLInstance.initPeriod(config.ttl)
+				await TRLInstance.initStages(config.activeTime, config.claimTime)
+
+				Allowance = await AllowanceContract.new()
+				Balance = await BalanceContract.new(TRLInstance.address, Allowance.address, Vault.address)
+		})
+
+		it('Should calculate the balance', async() => {
+				// _calculateBalance(uint256 _entityAllowance, uint256 _periodPool)
+				const entityAllowance = 40
+				const periodPool = 200
+				const expectedAllowance = 80
+				const actualAllowance = await Balance._calculateBalance(entityAllowance, periodPool)
+				assert.equal(actualAllowance, expectedAllowance)
+		})
+
+		it('Should be period 0', async() => {
+				// _calculateBalance(uint256 _entityAllowance, uint256 _periodPool)
+
+				const balance = await Balance.getBalance(candidateAccounts[0], FrontierTokenInstance.address)
+				assert.equal(balance, 0)
+		})
+
+		it("Should calculate the correct amount", async()=>{
+				let vaultBalance = await Vault.balance(0, FrontierTokenInstance.address)
+				await Allowance.addEntity(entity1, "entity-1", entity1Allowance)
+				await Allowance.addEntity(entity2, "entity-2", entity2Allowance)
+				await Allowance.addEntity(entity3, "entity-3", entity3Allowance)
+				const setAllowance = await Allowance.getEntityAllowance(entity1)
+				await Balance.setBalancesForEntities([entity1,entity2,entity3], FrontierTokenInstance.address,{from: owner})
+				const setBalance1 = await Balance.getBalance (entity1, FrontierTokenInstance.address)
+				const setBalance2 = await Balance.getBalance (entity2, FrontierTokenInstance.address)
+				const setBalance3 = await Balance.getBalance (entity3, FrontierTokenInstance.address)
+				assert.equal(setBalance1,totalTokenIssuance*entity1Allowance/100,"Balance of entity"+1+"wrong")
+				assert.equal(setBalance2,totalTokenIssuance*entity2Allowance/100,"Balance of entity"+2+"wrong")
+				assert.equal(setBalance3,totalTokenIssuance*entity3Allowance/100,"Balance of entity"+3+"wrong")
+		})
+		
+		it("Should return the correct balance after withdraw", async()=>{
+				let vaultBalance = await Vault.balance(0, FrontierTokenInstance.address)
+				await Allowance.addEntity(entity1, "entity-1", entity1Allowance)
+				await Balance.setBalancesForEntities([entity1], FrontierTokenInstance.address,{from: owner})
+				const initialBalance = await Balance.getBalance (entity1, FrontierTokenInstance.address)
+				const withdrawAmount = 100
+				await Balance.withdraw(entity1, FrontierTokenInstance.address, withdrawAmount)
+				const afterBalance = await Balance.getBalance (entity1, FrontierTokenInstance.address)
+				
+				assert.equal(afterBalance,initialBalance-withdrawAmount,"Balance after withdrawal is wrong")
+		})
+		
+		it("Should fail when withdrawing too much", async()=>{
+				let vaultBalance = await Vault.balance(0, FrontierTokenInstance.address)
+				await Allowance.addEntity(entity1, "entity-1", entity1Allowance)
+				await Balance.setBalancesForEntities([entity1], FrontierTokenInstance.address,{from: owner})
+				const initialBalance = await Balance.getBalance (entity1, FrontierTokenInstance.address)
+				const withdrawAmount = initialBalance+1
+				await assertRevert( Balance.withdraw(entity1, FrontierTokenInstance.address, withdrawAmount))	
+		})
+
 })
