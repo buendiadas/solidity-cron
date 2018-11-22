f6da73772248064f27022fa3667765176c254c18
Carlos Buendía <cbuendia@influity.com>
2018-11-22 10:56:16 +0100

    fix(migrations): Deletes old monthly period migration

--- migrations/2_monthly_period.js
+++
@@ -1,9 +0,0 @@
-const MonthlyPeriod = artifacts.require('MonthlyPeriod.sol');
-const DateTime = artifacts.require('DateTime.sol');
-
-module.exports = function (deployer) {
-    deployer.deploy(DateTime).then(() => {
-        deployer.deploy(MonthlyPeriod);
-    });
-    deployer.link(DateTime, MonthlyPeriod);
-};
