2fd4da26a0a74c2501ee563deb76f8eac680a791
Carlos Buendía <cbuendia@influity.com>
2018-12-07 13:25:37 +0100

    ci(period): Modified link to Datetime to be syncronous

--- migrations/3_period.js
+++ migrations/3_period.js
@@ -2,10 +2,10 @@ const config = require('../config')
 const DateTime = artifacts.require('DateTime');
 const PeriodContract = artifacts.require('Daily');
 
-module.exports = (deployer) => {
-  deployer.then(async () => {
-  await deployer.deploy(DateTime)
-  await deployer.deploy(PeriodContract)
-  await deployer.link(DateTime, PeriodContract)
+module.exports = function (deployer) {
+  deployer.deploy(DateTime).then(() => {
+    deployer.deploy(PeriodContract)
   })
+  deployer.link(DateTime, PeriodContract)
 }
+
